26172aef8723a29a4f912acda42a54166cfe2484
Justin Capogna <jcapogna@gmail.com>
2021-05-25 21:48:11 -0500

    Update to use new SDK interface

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import AppFlags, {useFlag} from "appflags-react";
 
-AppFlags.initialize("YOUR_KEY_HERE");
+const user = {
+    key: Math.random().toString()  // key should be a non-changing string representing your user, like an ID or email
+}
+AppFlags.initialize("YOUR_KEY_HERE", user);
 
 function App() {
-    const enabled = useFlag("test");
+    const flag = useFlag("test");
 
     return (
         <div>
             <h2>AppFlags React Example</h2>
             <div>
-                { enabled === undefined ?
+                { flag === undefined ?
                     "Initializing..." :
-                    "The feature is " + (enabled ? "enabled" : "disabled")
+                    "The feature is " + (flag.value ? "enabled" : "disabled")
                 }
             </div>
         </div>
